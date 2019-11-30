b68a1315635efd08bf0cf6e530167e252cee7d9e
Iliana Meza <ilianameza530@gmail.com>
2019-11-30 13:03:51 -0800

    Add get ‘/*’ route with passing tests.
    Add tests for get '/*' route that test for 301 status, Location header
    and that the JSON response contains the originial long URL.
    Add get '/*' route in app.rb that gets the short url and locates the
    hash with the corresponding long url and redirects to it.

--- app.rb
+++ app.rb
@@ -5,22 +5,27 @@ require 'sinatra/json'
 require './lib/url_shortener'
 
 class URLShortener < Sinatra::Base
-    @@url_list = []
+  @@url_list = []
 
-    post '/shortener' do
-        @new_url = Shortener.new
-        @new_url = @new_url.generate_short_url
-        params = JSON.parse(request.body.read)
-        url = params['url']
-    
-        if !url.start_with?('http://') && !url.start_with?('https://')
-          url = 'http://' + url
-        end
-    
-        entry = { short_url: @new_url.to_s, url: url.to_s }
-    
-        @@url_list << entry
-        json entry
+  post '/shortener' do
+    @new_url = Shortener.new
+    @new_url = @new_url.generate_short_url
+    params = JSON.parse(request.body.read)
+    url = params['url']
+
+    if !url.start_with?('http://') && !url.start_with?('https://')
+      url = 'http://' + url
     end
-    
+
+    entry = { short_url: @new_url.to_s, url: url.to_s }
+
+    @@url_list << entry
+    json entry
+  end
+
+  get '/*' do
+    requested_short_url = "/#{params['splat'].first}"
+    entry = @@url_list.detect { |entry| entry[:short_url] == requested_short_url }
+    redirect to(entry[:url]), 301, json(url: entry[:url])
+  end
 end
