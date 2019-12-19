ef1b879543300ef8887474421408e6e94e8b34cf
Iliana Meza <ilianameza530@gmail.com>
2019-12-19 12:12:39 +0000

    refactor methods in app.rb

--- app.rb
+++ app.rb
@@ -5,6 +5,9 @@ require 'sinatra/json'
 require './lib/url_shortener'
 
 class URLShortener < Sinatra::Base
+
+  set :public_folder, File.dirname(__FILE__) + '/static'
+
   @@url_list = []
 
   get '/' do
@@ -12,24 +15,42 @@ class URLShortener < Sinatra::Base
   end
 
   post '/shortener' do
-    @new_url = Shortener.new
-    @new_url = @new_url.generate_short_url
-    params = JSON.parse(request.body.read)
-    url = params['url']
+      shortener = Shortener.new
+      short_url = shortener.generate_short_url
 
-    if !url.start_with?('http://') && !url.start_with?('https://')
-      url = 'http://' + url
-    end
+      begin
+        params = JSON.parse(request.body.read)
+      rescue
+        status 404
+        return json :error => 'Invalid request'
+      end
 
-    entry = { short_url: @new_url.to_s, url: url.to_s }
+      url = params['url']
 
-    @@url_list << entry
-    json entry
+      if url.empty?
+        status 404
+        return json :error => 'Invalid request'
+      end
+
+      if !url.start_with?('http://') && !url.start_with?('https://')
+        url = 'http://' + url
+      end
+
+      entry = { short_url: short_url.to_s, url: url.to_s }
+
+      @@url_list << entry
+      json entry
   end
 
   get '/*' do
     requested_short_url = "/#{params['splat'].first}"
     entry = @@url_list.detect { |entry| entry[:short_url] == requested_short_url }
+
+    if entry == nil
+      status 404
+      return json :error => 'This short URL could not be found'
+    end
+
     redirect to(entry[:url]), 301, json(url: entry[:url])
   end
 end
